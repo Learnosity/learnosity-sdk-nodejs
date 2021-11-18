ee601ff78e468cef16c298836e26a50e467d55fe
Edwin Dawson <edwin.dawson@learnosity.com>
2021-11-18 13:20:33 +1100

    [DOC] Fix quotes.

--- docs/quickstart/assessment/standalone-assessment.js
+++ docs/quickstart/assessment/standalone-assessment.js
@@ -27,11 +27,11 @@ app.get('/', function (req, res) {
         // Consumer key and consumer secret are the public & private security keys required to access Learnosity APIs and data. These keys grant access to Learnosity's public demos account. Learnosity will provide keys for your own account.
         {
             consumer_key: config.consumerKey, // Load key from config.js
-            domain: domain,                   // Set the domain (from line 19)
+            domain: domain,                   // Set the domain (from line 20)
         },
         config.consumerSecret,                // Load secret from config.js
         {
-            // Unique student identifier, a UUID generated on line 17.
+            // Unique student identifier, a UUID generated on line 18.
             user_id: user_id,
             // A reference of the Activity to retrieve from the Item bank, defining which Items will be served in this assessment.
             activity_template_id: 'quickstart_examples_activity_template_001',
@@ -39,13 +39,13 @@ app.get('/', function (req, res) {
             // Uniquely identifies this specific assessment attempt session for  save/resume, data retrieval and reporting purposes. A UUID generated on line 18.
             session_id: session_id,
             // Used in data retrieval and reporting to compare results with other users submitting the same assessment.
-            activity_id: "quickstart_examples_activity_001",
+            activity_id: 'quickstart_examples_activity_001',
             // Selects a rendering mode, `assess` type is a "standalone" mode (loading a complete assessment player for navigation, VS `inline`, for embedded).
             rendering_type: 'assess',
             // Selects the context for the student response storage `submit_practice` mode means student response storage in the Learnosity cloud, for grading.
             type: 'submit_practice',
             // Human-friendly display name to be shown in reporting.
-            name: "Items API Quickstart",
+            name: 'Items API Quickstart',
             // Can be set to `initial, `resume` or `review`. Optional. Default = `initial`.
             state: 'initial',
         }
